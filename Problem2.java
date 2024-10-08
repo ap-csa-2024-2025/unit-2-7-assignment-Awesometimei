5e146f2cc2b10003afc0ab9c77631fbe81f81d08
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-10-08 10:15:10 -0700

    danny

--- Problem2.java
+++ Problem2.java
@@ -8,10 +8,10 @@ public class Problem2
     Integer x;
     Integer y;
 
-    System.out.println(x + y);
     System.out.println("Enter values:");
-    x = scan.nextLong();
+    x = scan.nextInt();
     y = scan.nextInt();
+    System.out.println(x + y);
 
     Double avg = (double)(x + y / 2);
     System.out.println("Average of " + x + " and " + y + " is " + avg);
