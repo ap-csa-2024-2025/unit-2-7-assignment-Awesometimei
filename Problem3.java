9b62ce87b270cfad61a09246abf34a6164d7c7ee
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-10-10 05:16:01 -0700

    danny

--- Problem3.java
+++ Problem3.java
@@ -13,8 +13,8 @@ public class Problem3
     System.out.println("Enter side length:");
     length = scan.nextDouble();
     
-    RegularPolygon p1 = new RegularPolygon(length, sides);
-    RegularPolygon p2 = new RegularPolygon(length + 1, sides * 2);
+    RegularPolygon p1 = new RegularPolygon(sides, length);
+    RegularPolygon p2 = new RegularPolygon(sides + 1, length * 2);
     
     System.out.println("The area of a " + p1 + " is: " + p1.getArea());
     System.out.println("The area of a " + p2 + " is: " + p2.getArea());
